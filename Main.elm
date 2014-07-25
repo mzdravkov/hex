558d6e56ad5d78c8ef29639e41d7178572d59dbb
mzdravkov <mihail0zdravkov@gmail.com>
2014-07-25 21:57:48 +0300

    functions for finding cell ranges

--- Main.elm
+++ Main.elm
@@ -11,19 +11,36 @@ clearGrey = rgba 111 111 111 0.6
 hexagon dimensions = outlined (solid clearGrey) (ngon 6 <| size dimensions)
 lineOfHexagons n dimensions = let hexagons = repeat n <| rotate (degrees 30) <| hexagon dimensions
                               in zipWith (\x h -> moveX x h)
-                                         (map (\n -> toFloat n * (size dimensions)*(sqrt 3)) [0..n-1])
+                                         (map (\i -> inLineOffset i (size dimensions)) [0..n-1])
                                          hexagons
 
 -- if you wonder what these magic numbers in the offsets are
 -- get a list of paper and do some geometry
--- (but +s*7.5 is a magic number, I have to admit)
+-- (but +s*7.5 and -5*s are magic numbers, I have to admit)
 newField n dimensions = let lines = repeat n <| lineOfHexagons n dimensions
                             s = size dimensions
-                            offsetX = (map (\n -> toFloat n * -((sqrt 3)/2)*s) [0..n-1])
-                            offsetY = (map (\n -> toFloat n * -1.5*s + 7.5*s) [0..n-1])
+                            offX = map (\row -> offsetX row s) [0..n-1]
+                            offY = map (\row -> offsetY row s) [0..n-1]
                         in zipWith (\offset line -> map (\cell -> move offset cell) line)
-                                   (zipWith (,) offsetX offsetY)
+                                   (zipWith (,) offX offY)
                                    lines
+
+inLineOffset cell size = toFloat cell*size*(sqrt 3)
+
+-- these are offsets for a whole row
+-- s = size
+offsetX row s = toFloat row * -((sqrt 3)/2)*s - 5*s
+offsetY row s = toFloat row * -1.5*s + 7.5*s
+
+cellXRanges s = map (\row -> map (\i -> let offX = offsetX row s
+                                        in (offX - s*(sqrt 3)/2, offX + s*(sqrt 3)/2))
+                                 [0..side-1])
+                    [0..side-1]
+
+cellYRanges s = map (\row -> map (\i -> let offY = offsetY row s
+                                        in (offY - s*(sqrt 3)/2, offY + s*(sqrt 3)/2))
+                                 [0..side-1])
+                    [0..side-1]
 
 -- these are some simle formulas for the radius of one hexagon,
 -- based on the size of the window
@@ -32,5 +49,10 @@ newField n dimensions = let lines = repeat n <| lineOfHexagons n dimensions
 size (w, h) = min (2*h/(3*side)) (w/(2 * side * sqrt 3))
 
 resizeableField (w, h) = collage w h <| map group <| newField side (toFloat w, toFloat h)
+--fieldWidth (w, h) = floor <| 2 * sqrt 3 * size (w, h) * side
+--fieldHeight (w, h) = floor <| 3*(size (w, h))*side / 2
+--c : (Int, Int) -> Element
+--c (w, h) = container (fieldWidth (toFloat w, toFloat h)) (fieldHeight (toFloat w, toFloat h)) middle <| resizeableField (w, h)
 
+-- main = lift (\(w, h) -> asText <| cellXRanges <| size (toFloat w, toFloat h)) Window.dimensions
 main = lift resizeableField Window.dimensions
